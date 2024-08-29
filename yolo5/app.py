f9dc3d31cdd2835fccfdb493ca015a2101fe6036
shantalberkhof <shantal.berkhof@gmail.com>
2024-08-29 20:36:10 +0300

    YOLO5 CICD + Commit YOLO5

--- yolo5/app.py
+++ yolo5/app.py
@@ -23,7 +23,7 @@ s3_client = boto3.client('s3', region_name='us-east-2')
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 table = dynamodb.Table('shantal-dynamoDB-aws') # Set the table name
 
-# A CHANGE 6
+# A CHANGE 7
 with open("data/coco128.yaml", "r") as stream:
     names = yaml.safe_load(stream)['names']
 
