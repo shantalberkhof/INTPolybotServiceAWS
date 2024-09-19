20d09b8df26c86085206f3cbd4dcdb3fcc8f474b
shantalberkhof <shantal.berkhof@gmail.com>
2024-09-19 19:42:23 +0300

    TF changes

--- yolo5/app.py
+++ yolo5/app.py
@@ -25,7 +25,6 @@ dynamodb = boto3.resource('dynamodb', region_name=REGION_NAME)
 table = dynamodb.Table(DYNAMODB_TABLE)
 # table = dynamodb.Table('shantal-dynamoDB-aws') # Set the table name
 
-
 # test 1
 with open("data/coco128.yaml", "r") as stream:
     names = yaml.safe_load(stream)['names']
